64720bc86416f2926a6d84df1de7c73d68910920
Illia Savinov <dresar2404@gmail.com>
2026-02-13 17:54:21 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_input() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_age_less_than_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_age_is_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_age_when_more_than_15_but_less_that_24() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_age_is_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_different_result_for_dog_and_cat() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_big_numbers() -> None:
+    assert get_human_age(100, 100) == [21, 17]
